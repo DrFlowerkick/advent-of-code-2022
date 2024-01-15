37710d60691720de3954963015b532d0b4e7d220
DrFlowerkick <flowerkick14@gmail.com>
2024-01-15 12:13:49 +0100

    Setup Day 03

---
+++ src/days/day_03.rs
@@ -0,0 +1,18 @@
+//!day_03.rs
+
+use anyhow::Result;
+
+pub fn day_03() -> Result<()> {
+    let input = include_str!("../../assets/day_03.txt");
+
+    let result_part1 = 0;
+    println!("result day 03 part 1: {}", result_part1);
+    //assert_eq!(result_part1, 74_711);
+
+    /*
+    let result_part2: u64 = max_calories[max_calories.len() - 3..].iter().sum();
+    println!("result day 01 part 2: {}", result_part2);
+    assert_eq!(result_part2, 209_481);
+    */
+    Ok(())
+}
