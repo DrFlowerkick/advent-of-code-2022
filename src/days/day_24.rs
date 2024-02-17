7b547ec1002343edc595311d3a892403980305e2
DrFlowerkick <flowerkick14@gmail.com>
2024-02-18 00:27:55 +0100

    Completed Day 24

--- src/days/day_24.rs
+++ src/days/day_24.rs
@@ -1,156 +1,197 @@
 //!day_24.rs
 
 use anyhow::Result;
-use std::cmp::Ordering;
+use num::integer::lcm;
 use std::collections::BTreeSet;
 
-use my_lib::{my_compass::Compass, my_map_point::MapPoint, my_map_two_dim::MyMap2D};
+use my_lib::{my_compass::Compass, my_geometry::my_point::Point};
 
-// values taken from ../../assets/day_24.txt
-const X: usize = 122;
-const Y: usize = 27;
-
-#[derive(Clone, Copy, Default, PartialEq, Eq)]
-struct ExpeditionState<const X: usize, const Y: usize> {
-    vale: MyMap2D<u8, X, Y>,
-    position: MapPoint<X, Y>,
-    minutes: usize,
+#[derive(Default)]
+struct BlizzardVale {
+    north_blizzards: Vec<Point>,
+    east_blizzards: Vec<Point>,
+    south_blizzards: Vec<Point>,
+    west_blizzards: Vec<Point>,
+    num_rows: i64,
+    num_columns: i64,
+    lcm_rc: i64,
 }
 
-impl<const X: usize, const Y: usize> From<&str> for ExpeditionState<X, Y> {
+impl From<&str> for BlizzardVale {
     fn from(value: &str) -> Self {
-        let mut vale: MyMap2D<u8, X, Y> = MyMap2D::default();
-        for (y, line) in value.lines().enumerate() {
-            for (x, v) in line.chars().enumerate() {
-                let map_pos = MapPoint::<X, Y>::new(x, y);
-                let value = match v {
-                    '#' => 255_u8,
-                    '.' => 0,
-                    '^' => 1,
-                    '>' => 2,
-                    'v' => 4,
-                    '<' => 8,
-                    _ => panic!("bad input"),
-                };
-                vale.set(map_pos, value);
-            }
-        }
-        let position = Self::START_POS;
-        *vale.get_mut(position) = Self::EXPEDITION_BIT_MASK;
-        Self {
-            vale,
-            position,
-            minutes: 0,
-        }
-    }
-}
-
-impl<const X: usize, const Y: usize> PartialOrd for ExpeditionState<X, Y> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<const X: usize, const Y: usize> Ord for ExpeditionState<X, Y> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.minutes.cmp(&other.minutes) {
-            Ordering::Greater => Ordering::Greater,
-            Ordering::Less => Ordering::Less,
-            Ordering::Equal => {
-                let delta_self = Self::END_POS.distance(self.position);
-                let delta_other = Self::END_POS.distance(other.position);
-                match delta_self.cmp(&delta_other) {
-                    Ordering::Greater => Ordering::Greater,
-                    Ordering::Less => Ordering::Less,
-                    Ordering::Equal => {
-                        if self.vale == other.vale {
-                            return Ordering::Equal;
-                        }
-                        //self.calculate_hash().cmp(&other.calculate_hash())
-                        Ordering::Less
-                    }
-                }
-            },
-        }
-    }
-}
-
-impl<const X: usize, const Y: usize> ExpeditionState<X, Y> {
-    const EXPEDITION_BIT_MASK: u8 = 16;
-    const START_POS: MapPoint<X, Y> = MapPoint::new_const(1, 0);
-    const END_POS: MapPoint<X, Y> = MapPoint::new_const(X - 2, Y - 1);
-    fn moving_blizzards(&self) -> Self {
-        let mut next_state = *self;
-        for (position, blizzards) in self
-            .vale
-            .iter()
-            .filter(|(_, b)| **b > 0 && **b < Self::EXPEDITION_BIT_MASK)
+        let mut blizzard_vale = BlizzardVale::default();
+        for (y, line) in value
+            .lines()
+            .skip(1)
+            .enumerate()
+            .map(|(y, l)| (y as i64, l))
         {
-            for blizzard in Compass::from_u8(*blizzards).iter() {
-                let bit_mask = blizzard.to_u8();
-                let new_position = position.neighbor(*blizzard).unwrap();
-                let new_position = match new_position.map_position() {
-                    Compass::Center => new_position,
-                    Compass::N | Compass::S => position.invert_y(),
-                    Compass::E | Compass::W => position.invert_x(),
-                    _ => panic!("internal error"),
-                };
-                *next_state.vale.get_mut(position) -= bit_mask;
-                *next_state.vale.get_mut(new_position) += bit_mask;
+            for (x, c) in line.chars().skip(1).enumerate().map(|(x, c)| (x as i64, c)) {
+                match c {
+                    '^' => blizzard_vale.north_blizzards.push((x, y).into()),
+                    '>' => blizzard_vale.east_blizzards.push((x, y).into()),
+                    'v' => blizzard_vale.south_blizzards.push((x, y).into()),
+                    '<' => blizzard_vale.west_blizzards.push((x, y).into()),
+                    _ => (),
+                }
+                blizzard_vale.num_columns = x;
+            }
+            blizzard_vale.num_rows = y;
+        }
+        blizzard_vale.lcm_rc = lcm(blizzard_vale.num_columns, blizzard_vale.num_rows);
+        blizzard_vale
+    }
+}
+
+impl BlizzardVale {
+    fn is_start_pos(&self, state: ExpeditionState) -> bool {
+        (state.x, state.y) == (0, -1)
+    }
+    fn is_end_pos(&self, state: ExpeditionState) -> bool {
+        (state.x, state.y) == (self.num_columns - 1, self.num_rows)
+    }
+    fn is_out_of_vale(&self, state: ExpeditionState) -> bool {
+        (state.x < 0 || state.y < 0 || state.x >= self.num_columns || state.y >= self.num_rows)
+            && !self.is_start_pos(state)
+            && !self.is_end_pos(state)
+    }
+    fn check_pos_for_blizzards(&self, state: ExpeditionState) -> bool {
+        if self.is_start_pos(state) || self.is_end_pos(state) {
+            return false;
+        }
+        let pos: Point = (state.x, state.y).into();
+        for dir in Compass::cardinals().into_iter() {
+            // shift pos in negativ direction of dir
+            let mut shifted_pos = pos.subtract(Point::from(dir).scale(state.minutes));
+            // modulo of shifted pos with size of vale (which means that pos always stays inside of vale)
+            match dir {
+                Compass::N | Compass::S => shifted_pos.y = shifted_pos.y.rem_euclid(self.num_rows),
+                Compass::E | Compass::W => {
+                    shifted_pos.x = shifted_pos.x.rem_euclid(self.num_columns)
+                }
+                _ => (),
+            }
+            // check if shifted_pos collides with a blizzard
+            if match dir {
+                Compass::N => self.north_blizzards.contains(&shifted_pos),
+                Compass::E => self.east_blizzards.contains(&shifted_pos),
+                Compass::S => self.south_blizzards.contains(&shifted_pos),
+                Compass::W => self.west_blizzards.contains(&shifted_pos),
+                _ => false,
+            } {
+                return true;
             }
         }
-        next_state.minutes += 1;
-        next_state
+        false
     }
-    fn set_new_position(&self, new_position: MapPoint<X, Y>) -> Self {
-        let mut new_position_state = *self;
-        *new_position_state.vale.get_mut(new_position_state.position) -= Self::EXPEDITION_BIT_MASK;
-        *new_position_state.vale.get_mut(new_position) += Self::EXPEDITION_BIT_MASK;
-        new_position_state.position = new_position;
-        new_position_state
-    }
-    fn moving_expedition(&self) -> Vec<Self> {
-        let mut new_positions = self
-            .position
-            .iter_neighbors(Compass::N, true, false, false)
-            .filter(|(p, _)| *self.vale.get(*p) == 0)
-            .map(|(p, _)| self.set_new_position(p))
-            .collect::<Vec<Self>>();
-        if *self.vale.get(self.position) == Self::EXPEDITION_BIT_MASK {
-            // add state for waiting, if current position stays free of blizzards
-            new_positions.push(*self);
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct ExpeditionState {
+    minutes: i64,
+    x: i64,
+    y: i64,
+    phase: i64,
+}
+
+impl ExpeditionState {
+    fn new() -> Self {
+        Self {
+            minutes: 0,
+            x: 0,
+            y: -1,
+            phase: 0,
         }
-        new_positions
     }
-    fn shortest_path_expedition(&self) -> usize {
-        let mut processing_states: BTreeSet<Self> = BTreeSet::new();
-        let mut seen_states: Vec<MyMap2D<u8, X, Y>> = Vec::new();
-        processing_states.insert(*self);
-        seen_states.push(self.vale);
-        while let Some(current_state) = processing_states.pop_first() {
-            let moved_blizzards = current_state.moving_blizzards();
-            for next_state in moved_blizzards.moving_expedition().iter() {
-                if next_state.position == Self::END_POS {
-                    return next_state.minutes;
-                }
-                if seen_states.contains(&next_state.vale) {
+    fn increment_phase(&self) -> Self {
+        Self {
+            minutes: self.minutes,
+            x: self.x,
+            y: self.y,
+            phase: self.phase + 1,
+        }
+    }
+    fn increment_minutes(&mut self) {
+        self.minutes += 1;
+    }
+    fn check_cycle(&self, lcm_rc: i64) -> Self {
+        Self {
+            minutes: self.minutes % lcm_rc,
+            x: self.x,
+            y: self.y,
+            phase: self.phase,
+        }
+    }
+    fn shift_pos(&self, pos: Point) -> Self {
+        Self {
+            minutes: self.minutes,
+            x: self.x + pos.x,
+            y: self.y + pos.y,
+            phase: self.phase,
+        }
+    }
+    fn shortest_path_expedition(&mut self, blizzard_vale: &BlizzardVale, num_phases: i64) -> i64 {
+        let mut queue: BTreeSet<Self> = BTreeSet::new();
+        queue.insert(*self);
+        let mut seen: Vec<Self> = Vec::new();
+        let mut current_phase = self.phase;
+        while let Some(mut current_state) = queue.pop_first() {
+            if current_state.phase < current_phase {
+                continue;
+            }
+            current_state.increment_minutes();
+            for new_state in Compass::center_and_cardinals()
+                .into_iter()
+                .map(|c| current_state.shift_pos(Point::from(c)))
+            {
+                // reached end of phase?
+                if (new_state.phase & 1 == 0 && blizzard_vale.is_end_pos(new_state))
+                    || (new_state.phase & 1 == 1 && blizzard_vale.is_start_pos(new_state))
+                {
+                    let phase_end_state = new_state.increment_phase();
+                    if phase_end_state.phase == num_phases {
+                        *self = phase_end_state;
+                        return new_state.minutes;
+                    }
+                    seen.push(phase_end_state);
+                    queue.insert(phase_end_state);
+                    current_phase = phase_end_state.phase;
                     continue;
                 }
-                processing_states.insert(*next_state);
-                seen_states.push(next_state.vale);
+                // check for out of map position and blizzards
+                if blizzard_vale.is_out_of_vale(new_state)
+                    || blizzard_vale.check_pos_for_blizzards(new_state)
+                {
+                    continue;
+                }
+                // check if new_state is cycle of previous state
+                let seen_state = new_state.check_cycle(blizzard_vale.lcm_rc);
+                if seen.contains(&seen_state) {
+                    continue;
+                }
+
+                seen.push(seen_state);
+                queue.insert(new_state);
             }
         }
-        // if there is no more state to process, while loop ends, which should not happen before END_POS is reached
-        0
+        // this should never happen
+        -1
     }
 }
+
+// So inspired (again) by HyperNeutrino
+// https://www.youtube.com/watch?v=R_QWG-cPp_k&list=PLnNm9syGLD3yf-YW-a5XNh1CJN07xr0Kz&index=24
 
 pub fn day_24() -> Result<()> {
     let input = include_str!("../../assets/day_24.txt");
-    let expedition = ExpeditionState::<X, Y>::from(input);
-    let result_part1 = expedition.shortest_path_expedition();
+    let blizzard_vale = BlizzardVale::from(input);
+    let mut expedition = ExpeditionState::new();
+    let result_part1 = expedition.shortest_path_expedition(&blizzard_vale, 1);
     println!("result day 24 part 1: {}", result_part1);
-    //assert_eq!(result_part1, 4_034);
+    assert_eq!(result_part1, 274);
+    let result_part2 = expedition.shortest_path_expedition(&blizzard_vale, 3);
+    println!("result day 24 part 2: {}", result_part2);
+    assert_eq!(result_part2, 839);
 
     Ok(())
 }
@@ -160,34 +201,6 @@ mod tests {
 
     use super::*;
 
-    const XB: usize = 7;
-    const YB: usize = 7;
-
-    const XD: usize = 8;
-    const YD: usize = 6;
-
-    #[test]
-    fn test_blizzard_moving() {
-        let input = "#.#####\n\
-                           #.....#\n\
-                           #>....#\n\
-                           #.....#\n\
-                           #...v.#\n\
-                           #.....#\n\
-                           #####.#";
-        let expedition = ExpeditionState::<XB, YB>::from(input);
-        eprintln!("{}", expedition.vale);
-        let blizzards_moving = expedition.moving_blizzards();
-        let blizzards_moving = blizzards_moving.moving_blizzards();
-        let blizzards_moving = blizzards_moving.moving_blizzards();
-        assert_eq!(*blizzards_moving.vale.get((4, 2).into()), 6);
-        eprintln!("{}", blizzards_moving.vale);
-        let blizzards_moving = blizzards_moving.moving_blizzards();
-        let blizzards_moving = blizzards_moving.moving_blizzards();
-        assert_eq!(blizzards_moving.vale, expedition.vale);
-        eprintln!("{}", blizzards_moving.vale);
-    }
-
     #[test]
     fn test_example() -> Result<()> {
         let input = "#.######\n\
@@ -196,10 +209,15 @@ mod tests {
                            #>v.><>#\n\
                            #<^v^^>#\n\
                            ######.#";
-        let expedition = ExpeditionState::<XD, YD>::from(input);
-        let result_part1 = expedition.shortest_path_expedition();
+        let blizzard_vale = BlizzardVale::from(input);
+        let mut expedition = ExpeditionState::new();
+        let result_part1 = expedition.shortest_path_expedition(&blizzard_vale, 1);
         println!("result example day 24 part 1: {}", result_part1);
         assert_eq!(result_part1, 18);
+        let result_part2 = expedition.shortest_path_expedition(&blizzard_vale, 3);
+        println!("result example day 24 part 2: {}", result_part2);
+        assert_eq!(result_part2, 54);
+
         Ok(())
     }
 }
